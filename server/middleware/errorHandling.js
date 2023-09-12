f457566461051bf585a42fe108b2d096baf2346f
Andreev Dmitry <91731413+Pro100Dimka@users.noreply.github.com>
2023-09-12 09:27:58 +0300

    a

--- server/middleware/errorHandling.js
+++
@@ -1,9 +0,0 @@
-const apiErrors = require('../errors/api');
-
-// Вывод ошибки если апишка не сработала
-module.exports = function (err, req, res, next) {
-  if (err instanceof apiErrors) {
-    return res.status(err.status).json({ message: err.message });
-  }
-  return res.status(500).json({ message: 'Не передбачуванна помилка' });
-};
